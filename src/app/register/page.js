4d2c905543e4ff4e34304f1398a994eded77e1b4
asagohan2301 <shemomechama@yahoo.co.jp>
2024-01-24 16:11:46 +0900

    ユーザー登録機能を実装しました

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,4 +1,53 @@
-export default function Resister() {
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Register() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const router = useRouter();
+
+  function onChangeUsername(e) {
+    setUsername(e.target.value);
+  }
+  function onChangeEmail(e) {
+    setEmail(e.target.value);
+  }
+  function onChangePassword(e) {
+    setPassword(e.target.value);
+  }
+
+  async function onSubmitRegister(e) {
+    e.preventDefault();
+
+    const user = {
+      user: {
+        username: username,
+        email: email,
+        password: password
+      }
+    }
+
+    const res = await fetch("http://localhost:3000/api/users", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(user)
+    });
+
+    const data = await res.json();
+    if (res.ok) {
+      router.push("/login");
+    } else {
+      setMessages(data.errors);
+    }
+  }
+
   return (
     <div className="auth-page">
       <div className="container page">
@@ -10,18 +59,20 @@ export default function Resister() {
             </p>
 
             <ul className="error-messages">
-              <li>That email is already taken</li>
+              {messages.map((message, index) => {
+                return <li key={index}>{message}</li>
+              })}
             </ul>
 
-            <form>
+            <form onSubmit={onSubmitRegister}>
               <fieldset className="form-group">
-                <input className="form-control form-control-lg" type="text" placeholder="Username" />
+                <input className="form-control form-control-lg" type="text" placeholder="Username" onChange={onChangeUsername} value={username} />
               </fieldset>
               <fieldset className="form-group">
-                <input className="form-control form-control-lg" type="text" placeholder="Email" />
+                <input className="form-control form-control-lg" type="text" placeholder="Email" onChange={onChangeEmail} value={email} />
               </fieldset>
               <fieldset className="form-group">
-                <input className="form-control form-control-lg" type="password" placeholder="Password" />
+                <input className="form-control form-control-lg" type="password" placeholder="Password" onChange={onChangePassword} value={password} />
               </fieldset>
               <button className="btn btn-lg btn-primary pull-xs-right">Sign up</button>
             </form>
