2a040fa771c27690eb740e7f916f8265b3a5b5c0
asagohan2301 <shemomechama@yahoo.co.jp>
2024-01-25 10:50:31 +0900

    ログイン機能を実装しました

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,6 +1,51 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
+import { setCookie } from "../lib/actions";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const router = useRouter();
+
+  function onChangeEmail(e) {
+    setEmail(e.target.value);
+  }
+  function onChangePassword(e) {
+    setPassword(e.target.value);
+  }
+
+  async function onSubmitLogin(e) {
+    e.preventDefault();
+
+    const user = {
+      user: {
+        email: email,
+        password: password
+      }
+    }
+
+    const res = await fetch("http://localhost:3000/api/users/login", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(user)
+    });
+
+    const data = await res.json();
+    if (res.ok) {
+      setCookie("token", data.user.token);
+      router.push("/");
+    } else {
+      setMessages(data.errors);
+    }
+  }
+
   return (
     <div className="auth-page">
       <div className="container page">
@@ -12,15 +57,17 @@ export default function Login() {
             </p>
 
             <ul className="error-messages">
-              <li>That email is already taken</li>
+              {messages.map((message, index) => {
+                return <li key={index}>{message}</li>
+              })}
             </ul>
 
-            <form>
+            <form onSubmit={onSubmitLogin}>
               <fieldset className="form-group">
-                <input className="form-control form-control-lg" type="text" placeholder="Email" />
+                <input className="form-control form-control-lg" type="text" placeholder="Email" onChange={onChangeEmail} value={email} />
               </fieldset>
               <fieldset className="form-group">
-                <input className="form-control form-control-lg" type="password" placeholder="Password" />
+                <input className="form-control form-control-lg" type="password" placeholder="Password" onChange={onChangePassword} value={password} />
               </fieldset>
               <button className="btn btn-lg btn-primary pull-xs-right">Sign in</button>
             </form>
