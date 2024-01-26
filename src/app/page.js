75e992a7c5ee1af258588ae04a08d803b6cc0cb9
asagohan2301 <shemomechama@yahoo.co.jp>
2024-01-26 21:46:39 +0900

    記事一覧表示機能を追加しました

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,21 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Link from "next/link";
 
 export default function Home() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/articles")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setArticles(data.articles);
+      })
+  }, []);
+
   return (
     <div className="home-page">
       <div className="banner">
@@ -24,27 +39,32 @@ export default function Home() {
               </ul>
             </div>
 
-            <div className="article-preview">
-              <div className="article-meta">
-                <Link href=""><img src="http://i.imgur.com/Qr71crq.jpg" /></Link>
-                <div className="info">
-                  <Link href="" className="author">author</Link>
-                  <span className="date">date</span>
+            {articles && articles.map((article, index) => {
+              return (
+                <div className="article-preview" key={index}>
+                  <div className="article-meta">
+                    <Link href=""><img src="http://i.imgur.com/Qr71crq.jpg" /></Link>
+                    <div className="info">
+                      <Link href="" className="author">{article.author.username}</Link>
+                      <span className="date">{article.created_at}</span>
+                    </div>
+                    <button className="btn btn-outline-primary btn-sm pull-xs-right">
+                      <i className="ion-heart"></i> 29
+                    </button>
+                  </div>
+                  <Link href={`/article/${article.slug}`} className="preview-link">
+                    <h2>{article.title}</h2>
+                    <p>{article.description}</p>
+                    <span>Read more...</span>
+                    <ul className="tag-list">
+                      {article.tag_list.map((tag, tag_index) => {
+                        return <li className="tag-default tag-pill tag-outline" key={tag_index}>{tag}</li>
+                      })}
+                    </ul>
+                  </Link>
                 </div>
-                <button className="btn btn-outline-primary btn-sm pull-xs-right">
-                  <i className="ion-heart"></i> 29
-                </button>
-              </div>
-              <Link href="" className="preview-link">
-                <h2>title</h2>
-                <p>description</p>
-                <span>Read more...</span>
-                <ul className="tag-list">
-                  <li className="tag-default tag-pill tag-outline">realworld</li>
-                  <li className="tag-default tag-pill tag-outline">implementations</li>
-                </ul>
-              </Link>
-            </div>
+              )
+            })}
 
             <ul className="pagination">
               <li className="page-item active">
